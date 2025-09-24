6f7eb327d96d15714dbf218248c8dd98e05cd116
Adeoye Dipo <adeoyedipo@Adeoyes-MacBook-Pro.local>
2025-09-24 13:37:15 -0230

    QA assessment and rewrites to nc

--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -21,8 +21,10 @@ def read_modis(file_path, var_name):
     return data
 
 
-
 def inverse_maps(H,V,size):
+    """
+    Converts from MODIS Sinusoidal projection to geographic coordinates (lat/lon)
+    """
     Lat=np.zeros((size,size))
     Lon=np.zeros((size,size))
     R=6371007.181
@@ -39,69 +41,75 @@ def inverse_maps(H,V,size):
             Lon[i,j]=180/np.pi*(xx/(R*np.cos(ll)))
     return Lat, Lon
 
-# def LST_extraction(input_folder, var, qc_var, varo, output_folder):
-#     os.makedirs(output_folder, exist_ok=True)
-#     filenames=sorted([f for f in os.listdir(input_folder) if f.endswith(".hdf")])
-#     for fil in filenames:
-#         file=os.path.join(input_folder, fil)
-#         output_path=os.path.join(output_folder, fil.replace(".hdf", ".nc"))
-#         LST_1km     = read_modis(file, var).data
-#         QC          = read_modis(file, qc_var).data
+
+
+def LST_extraction(input_folder, var, qc_var, varo, output_folder):
+    """
+    Performs Quality Assurance assessment on each pixel using MODIS QC flags.
+    Pixels that fail quality criteria OR have 0 values are replaced with NaN.
+    """
+    os.makedirs(output_folder, exist_ok=True)
+    filenames=sorted([f for f in os.listdir(input_folder) if f.endswith(".hdf")])
+    for fil in filenames:
+        file=os.path.join(input_folder, fil)
+        output_path=os.path.join(output_folder, fil.replace(".hdf", ".nc"))
+        LST_1km     = read_modis(file, var).data
+        QC          = read_modis(file, qc_var).data
     
-#     	ql1 = (QC.astype('uint8') & 0b00000011) >> 0 # Mandatory quality flag
-#         ql2 = (QC.astype('uint8') & 0b00001100) >> 2 # Data quality flag
-#         ql3 = (QC.astype('uint8') & 0b00110000) >> 4  # Emissivity error flag
-#         ql4 = (QC.astype('uint8') & 0b01100000) >> 5 # LST error flag
+    	ql1 = (QC.astype('uint8') & 0b00000011) >> 0 # Mandatory quality flag
+        ql2 = (QC.astype('uint8') & 0b00001100) >> 2 # Data quality flag
+        ql3 = (QC.astype('uint8') & 0b00110000) >> 4  # Emissivity error flag
+        ql4 = (QC.astype('uint8') & 0b01100000) >> 5 # LST error flag
     
-#     	ql1_mask = np.where((ql1 == 0) | (ql1 == 1), 1, 0)
-#         ql2_mask = np.where((ql2 == 0) | (ql2 == 1), 1, 0)
-#         ql3_mask = np.where(ql3 == 0, 1, 0)
-#         ql4_mask = np.where(ql4 == 0, 1, 0)
+    	ql1_mask = np.where((ql1 == 0) | (ql1 == 1), 1, 0)
+        ql2_mask = np.where((ql2 == 0) | (ql2 == 1), 1, 0)
+        ql3_mask = np.where(ql3 == 0, 1, 0)
+        ql4_mask = np.where(ql4 == 0, 1, 0)
           
-#         total_mask = (ql1_mask & ql2_mask & ql3_mask & ql4_mask).astype(int)
-#         LST_1km_valid=LST_1km*total_mask
-#         LST_1km_valid[LST_1km_valid==0]=np.nan
+        total_mask = (ql1_mask & ql2_mask & ql3_mask & ql4_mask).astype(int)
+        LST_1km_valid=LST_1km*total_mask
+        LST_1km_valid[LST_1km_valid==0]=np.nan
           
-# 	lat, lon = inverse_maps(18, 7, 1200)
-#         x_res=np.linspace(lon.min(), lon.max(), lon.shape[0])
-#         y_res=np.linspace(lat.min(), lat.max(), lat.shape[0])
+	lat, lon = inverse_maps(18, 7, 1200)
+        x_res=np.linspace(lon.min(), lon.max(), lon.shape[0])
+        y_res=np.linspace(lat.min(), lat.max(), lat.shape[0])
         
-    
-#         year=int(fil.split(".")[1][1:5])
-#         doy=int(fil.split(".")[1][5:8])
-#         time=datetime.datetime(year, 1, 1) + timedelta(days=doy - 1)
-#         time_values=time
+        #  Save the changes as netcdf
+        year=int(fil.split(".")[1][1:5])
+        doy=int(fil.split(".")[1][5:8])
+        time=datetime.datetime(year, 1, 1) + timedelta(days=doy - 1)
+        time_values=time
           
-#         with Dataset(output_path, "w", format="NETCDF4") as nc_file:
-#             nc_file.createDimension("time", 1)
-#             nc_file.createDimension("lat", len(y_res))
-#             nc_file.createDimension("lon", len(x_res))
+        with Dataset(output_path, "w", format="NETCDF4") as nc_file:
+            nc_file.createDimension("time", 1)
+            nc_file.createDimension("lat", len(y_res))
+            nc_file.createDimension("lon", len(x_res))
             
-#             # Create coordinate variables
-#             times = nc_file.createVariable("time", "f4", ("time",))
-#             lat = nc_file.createVariable("lat", "f4", ("lat",))
-#             lon = nc_file.createVariable("lon", "f4", ("lon",))
+            # Create coordinate variables
+            times = nc_file.createVariable("time", "f4", ("time",))
+            lat = nc_file.createVariable("lat", "f4", ("lat",))
+            lon = nc_file.createVariable("lon", "f4", ("lon",))
             
-#             # Create variables
-#             variables = nc_file.createVariable(varo, "f4", ("time", "lat", "lon",))
+            # Create variables
+            variables = nc_file.createVariable(varo, "f4", ("time", "lat", "lon",))
             
-#             # Add attributes to coordinate variables
-#             times.units = "hours since 2001-01-01 00:00:00"
-#             times.calendar = "gregorian"
-#             lat.units = "degrees_north"
-#             lon.units = "degrees_east"
-#             lat.long_name = "Latitude"
-#             lon.long_name = "Longitude"
+            # Add attributes to coordinate variables
+            times.units = "hours since 2001-01-01 00:00:00"
+            times.calendar = "gregorian"
+            lat.units = "degrees_north"
+            lon.units = "degrees_east"
+            lat.long_name = "Latitude"
+            lon.long_name = "Longitude"
             
-#             # Add attributes to data variables
-#             variables.units = "k"
-#             variables.long_name = "Modis temperature"
+            # Add attributes to data variables
+            variables.units = "k"
+            variables.long_name = "Modis temperature"
             
-#             # Write data to coordinate variables
-#             times[:] = date2num(time_values, units=times.units, calendar=times.calendar)
-#             lat[:] = y_res
-#             lon[:] = x_res
+            # Write data to coordinate variables
+            times[:] = date2num(time_values, units=times.units, calendar=times.calendar)
+            lat[:] = y_res
+            lon[:] = x_res
             
-#             # Write data to data variables
-#             variables[:, :, :] = LST_1km_valid[::-1, :]
-#         print(f"file {fil} processed and saved as {output_path}")
+            # Write data to data variables
+            variables[:, :, :] = LST_1km_valid[::-1, :]
+        print(f"file {fil} processed and saved as {output_path}")
