74726d75d74f15cb9f7d5fd7129c460632f6fd0b
Adeoye Dipo <adeoyedipo@Adeoyes-MacBook-Pro.local>
2025-09-30 15:19:01 -0230

    deleted rewrite era5_skin_temp.py

--- preprocessing_era5/rewrite era5_skin_temp.py
+++
@@ -1,8 +0,0 @@
-from rewrite import rewrite_func
-
-input_dir="./download_era5/skin_temp_2022_2004_africa"
-output_dir="./skin_temperature_rewriten"
-temp_long_name = "Skin temperature"
-var_name = "skt"
-
-rewrite_func(input_dir,output_dir,var_name,temp_long_name)
